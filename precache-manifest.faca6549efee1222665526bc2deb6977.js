5c4fafabe9b2f9142bb033af372c3b9e0426e89a
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2021-06-21 21:05:27 +0300

    deploy

---
+++ precache-manifest.faca6549efee1222665526bc2deb6977.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9d6b3cf7ae8b230245",
+    "url": "/SimonGame/css/app.1ff0c534.css"
+  },
+  {
+    "revision": "7b779e6a7cf0960b75b34d5d9edb518a",
+    "url": "/SimonGame/index.html"
+  },
+  {
+    "revision": "0e9d6b3cf7ae8b230245",
+    "url": "/SimonGame/js/app.73591b5e.js"
+  },
+  {
+    "revision": "3a797a3c6fe88abde6c1",
+    "url": "/SimonGame/js/chunk-vendors.c10de2e0.js"
+  },
+  {
+    "revision": "92e1bbc5b69192872dca7b1a5b7201ae",
+    "url": "/SimonGame/manifest.json"
+  },
+  {
+    "revision": "99e50f7d6744bfe49381ee010a0ed8ad",
+    "url": "/SimonGame/media/sounds1.99e50f7d.mp3"
+  },
+  {
+    "revision": "d2c4a2c42f3ff79a6674c18fa42aba97",
+    "url": "/SimonGame/media/sounds_2.d2c4a2c4.mp3"
+  },
+  {
+    "revision": "c1c96b4438f582241dcdee51f5189b79",
+    "url": "/SimonGame/media/sounds_3.c1c96b44.mp3"
+  },
+  {
+    "revision": "36d5a7222077f946d6dbc028b5b8a8a6",
+    "url": "/SimonGame/media/sounds_4.36d5a722.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/SimonGame/robots.txt"
+  }
+]);
